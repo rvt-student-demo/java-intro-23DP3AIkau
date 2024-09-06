1c0cc06aae1e63af45b8c5f0181108356bc50e37
23DP3AIkau <166131595+23DP3AIkau@users.noreply.github.com>
2024-09-06 11:20:52 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,26 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadi savu vārdu: ");
+
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadi savu uzvārdu: ");
+        String lastname = scanner.nextLine();
+
+        System.out.println("Ievadi savu grupu: ");
+        String group = scanner.nextLine();
+
+        System.out.println("Students: " + name + " " + lastname + " Grupa: " + group);
+
+
 
     }
 
