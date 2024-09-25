b4fe22cf91a09b15fd2b51cdf5d305157fa329c0
23DP3AIkau <166131595+23DP3AIkau@users.noreply.github.com>
2024-09-25 07:13:14 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,18 +6,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        Scanner scanner = new Scanner(System.in);
+        //Scanner scanner = new Scanner(System.in);
 
         //System.out.println("Ievadi savu vārdu: ");
-
         //String name = scanner.nextLine();
-
         //System.out.println("Ievadi savu uzvārdu: ");
         //String lastname = scanner.nextLine();
-
         //System.out.println("Ievadi savu grupu: ");
         //String group = scanner.nextLine();
-
         //System.out.println("Students: " + name + " " + lastname + " Grupa: " + group);
 
 
@@ -43,13 +39,11 @@ public class App
         //System.out.println(e);
         
 
-
+        //Simple calculator
         // System.out.println("Give the first number:");
         // int firstn = Integer.valueOf(scanner.nextLine());
-
         // System.out.println("Give the second number:");
         // int second = Integer.valueOf(scanner.nextLine());
-
         // System.out.println(firstn + " + " + second + " = " + (firstn+second));
         // System.out.println(firstn + " - " + second + " = " + (firstn-second));
         // System.out.println(firstn + " * " + second + " = " + (firstn*second));
@@ -57,38 +51,60 @@ public class App
         // System.out.println(firstn + " un " + second + " Vidējais skaitlis = " + ((double)firstn+second)/2); //videjais
 
 
+        //Speed ticket
         // System.out.println("Give speed:");
         // int speed = Integer.valueOf(scanner.nextLine());
-
-
         // if (speed > 120) {
         //     System.out.println("Speeding ticket!");
         // }
 
 
 
-        
-        System.out.println("Give points [0-100]");
-        int grade = Integer.valueOf(scanner.nextLine());
-        if (grade < 0) {
-            System.out.println("Grade: impossible");
-        } else if (grade <= 49) {
-            System.out.println("Grade: failed");
-        } else if (grade <= 59) {
-            System.out.println("Grade: 1");
-        } else if (grade <= 69) {
-            System.out.println("Grade: 2");
-        } else if (grade <= 79) {
-            System.out.println("Grade: 3");
-        } else if (grade <= 89) {
-            System.out.println("Grade: 4");
-        } else if (grade <= 100) {
-            System.out.println("Grade: 5");
-        } else if (grade > 100) {
-            System.out.println("Grade: incredible!");
+        // Grades and Points
+        // System.out.println("Give points [0-100]");
+        // int grade = Integer.valueOf(scanner.nextLine());
+        // if (grade < 0) {
+        //     System.out.println("Grade: impossible");
+        // } else if (grade <= 49) {
+        //     System.out.println("Grade: failed");
+        // } else if (grade <= 59) {
+        //     System.out.println("Grade: 1");
+        // } else if (grade <= 69) {
+        //     System.out.println("Grade: 2");
+        // } else if (grade <= 79) {
+        //     System.out.println("Grade: 3");
+        // } else if (grade <= 89) {
+        //     System.out.println("Grade: 4");
+        // } else if (grade <= 100) {
+        //     System.out.println("Grade: 5");
+        // } else if (grade > 100) {
+        //     System.out.println("Grade: incredible!");
+        // }
+
+
+
+
+        //Gift Tax
+        Scanner scanner = new Scanner(System.in);
+        int money = Integer.valueOf(scanner.nextLine());
+        if (money < 5000) {
+            System.out.println("No tax!");
+        } 
+        else if (money >= 5000 && money <= 25000) {
+            System.out.println("Tax " + (100+(money-5000)*0.08));
         }
-        
-        
+        else if (money > 25000 && money <= 55000) {
+            System.out.println("Tax " + (1700+(money-25000)*0.1));
+        }
+        else if (money > 55000 && money <= 200000) {
+            System.out.println("Tax " + (4700+(money-55000)*0.12));
+        }
+        else if (money > 200000 && money <= 1000000) {
+            System.out.println("Tax " + (22100+(money-200000)*0.15));
+        }
+        else {
+            System.out.println("Tax " + (142100+(money-1000000)*0.17));
+        }
     }
 
 }
