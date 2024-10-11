613a7d9e34160c0cbc28f7549a256b7cb9628e3b
23DP3AIkau <166131595+23DP3AIkau@users.noreply.github.com>
2024-10-11 06:28:58 +0000

    Advanced astrology (3 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,14 +6,10 @@ public class App
 {
     public static void main( String[] args ) {
         // Scanner scanner = new Scanner(System.in);
-        Stars.printStars(5);
-        Stars.printStars(3);
-        Stars.printStars(9);
+        Stars.printTraingle(4);
         System.out.println("");
-        Stars.printSquare(4);
+        Stars.christmasTree(4);
         System.out.println("");
-        Stars.printRectangle(17,3);
-        System.out.println("");
-        Stars.printTriangle(4);
+        Stars.christmasTree(10);
     }
 }
--- src/main/java/lv/rvt/Stars.java
+++ src/main/java/lv/rvt/Stars.java
@@ -1,27 +1,34 @@
 package lv.rvt;
 
 public class Stars {
-    public static void printStars(int length) {
-        for (int i = 1; i <= length; i++){
+    public static void printStars(int number) {
+        for (int i = 1; i <= number; i++){
             System.out.print("*");
         }
         System.out.println("");
     }
 
-    public static void printSquare(int size) {
-        for (int i = 1; i <= size; i++) {
-            printStars(size);
+    public static void printSpaces(int number) {
+        for (int i = 1; i <= number; i++){
+            System.out.print(" ");
         }
     }
-    public static void printRectangle(int width, int height) {
-        for (int i = 1; i <= height; i++) {
-            printStars(width);
-        }
-    }
-    public static void printTriangle(int size) {
+    public static void printTraingle(int size) {
+        int spaces = size - 1;
         for (int i = 1; i <= size; i++) {
+            printSpaces(spaces);
             printStars(i);
+            spaces--;
+        }            
+    }
+    public static void christmasTree(int height) {
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars(2 * i - 1);
         }
-
+        for (int i = 1; i <= 2; i++) {
+            printSpaces(height - 2);
+            printStars(3);
+        }
     }
 }
