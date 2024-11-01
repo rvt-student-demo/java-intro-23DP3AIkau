c172b6efc1d9bee7c1bc022eaa91797569635bcc
23DP3AIkau <166131595+23DP3AIkau@users.noreply.github.com>
2024-11-01 07:18:46 +0000

    Personal details

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,32 +7,33 @@ public class App
 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
-        while (true){
-            int input = Integer.valueOf(scanner.nextLine());
-            if (input == -1){
+
+        String longest = "";
+        double count = 0;
+        int sum = 0;
+
+        while (true) {
+            String input = String.valueOf(scanner.nextLine());
+            if (input.equals("")) {
                 break;
             }
-            list.add(input);
-        }
+            String [] parts = input.split(",");
 
+            count++;
+            sum = sum + Integer.valueOf(parts[1]);
 
-        System.out.print("From where? ");
-        int start = Integer.valueOf(scanner.nextLine());
-
-        if (start >= 0 && start < list.size()){
-            System.out.print("To where? ");
-            int end = Integer.valueOf(scanner.nextLine());
-
-            if (end >= start && end < list.size()){
-                for (int i = start; i <= end; i++){
-                    System.out.println(list.get(i));
-                }
-            } else {
-                System.out.println("Nepareiza ievade");
+            
+            if (parts[0].length() > longest.length()){
+                longest = parts[0];
             }
-        } else {
-            System.out.println("Nepareiza ievade");
+
+            
+
+
         }
+        
+        System.out.println("Longest name: " + longest);
+        System.out.println("Average of the birth years: " + sum/count);
+
     }
 }
