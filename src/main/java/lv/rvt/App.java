22545e6d076a5322944b246491e6ee38fd98eb17
23DP3AIkau <166131595+23DP3AIkau@users.noreply.github.com>
2024-11-06 07:43:30 +0000

    Your first bank transfer

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,21 +8,22 @@ public class App
     public static void main(String[] args) {
         
 
-        Account artosAccount = new Account("Arto's account", 100.00);
-        Account artosSwissAccount = new Account("Arto's account in Switzerland", 1000000.00);
-            
+        Account matthewsAccount = new Account("Matthews account", 1000.00);
+        Account myAccount = new Account("My account", 0.00);
+
         System.out.println("Initial state");
-        System.out.println(artosAccount);
-        System.out.println(artosSwissAccount);
-            
-        artosAccount.withdraw(20);
-        System.out.println("The balance of Arto's account is now: " + artosAccount.balance());
-        artosSwissAccount.deposit(200);
-        System.out.println("The balance of Arto's other account is now: " + artosSwissAccount.balance());
-            
+        System.out.println(matthewsAccount);
+        System.out.println(myAccount);
+
+        matthewsAccount.withdraw(100);
+        System.out.println("The balance of Matthews account is now: " + matthewsAccount.balance());
+        myAccount.deposit(100);
+        System.out.println("The balance of my account is now: " + myAccount.balance());
+
         System.out.println("End state");
-        System.out.println(artosAccount);
-        System.out.println(artosSwissAccount);
+        System.out.println(matthewsAccount);
+        System.out.println(myAccount);
+
 
     }
 }
