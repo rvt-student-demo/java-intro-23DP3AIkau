54e3d3681a792f191f2c6c1674ff80a026023552
23DP3AIkau <166131595+23DP3AIkau@users.noreply.github.com>
2024-11-22 06:38:01 +0000

    Books

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,26 +1,41 @@
 package lv.rvt;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 
-public class App 
-{
+public class App {
+
     public static void main(String[] args) {
-        PaymentCard paulsCard = new PaymentCard(20);
-        PaymentCard mattsCard = new PaymentCard(30);
-        
-        paulsCard.eatHeartily();
-        mattsCard.eatAffordably();
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
-        paulsCard.addMoney(20.00);
-        mattsCard.eatHeartily();
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
-        paulsCard.eatAffordably();
-        paulsCard.eatAffordably();
-        mattsCard.addMoney(50);
-        System.out.println("Paul: " + paulsCard);
-        System.out.println("Matt: " + mattsCard);
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Books> books = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Title: ");
+            String title = scanner.nextLine();
+
+            if (title.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Pages: ");
+            int pages = Integer.valueOf(scanner.nextLine());
+
+            System.out.println("Publication year: ");
+            int year = Integer.valueOf(scanner.nextLine());
+
+            books.add(new Books(title, pages, year));
+        }
+
+        System.out.println("What information will be printed? ");
+        String information = scanner.nextLine();
+
+        for (Books book : books) {
+            if (information.equals("everything")) {
+                System.out.println(book);
+            } else if (information.equals("name")) {
+                System.out.println(book.getTitle());
+            }
+        }
     }
 }
