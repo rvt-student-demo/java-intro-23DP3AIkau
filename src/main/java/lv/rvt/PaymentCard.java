7f7c07ee53c5c37623c37de883f76458c6275d48
23DP3AIkau <166131595+23DP3AIkau@users.noreply.github.com>
2024-12-06 09:50:36 +0000

    Card payments (4 sections)

--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -7,32 +7,22 @@ public class PaymentCard {
         this.balance = openingBalance;
     }
 
-    public String toString() {
-        return "The card has a balance of " + this.balance + " euros";
+    public double balance() {
+        return this.balance;
     }
 
-    public void eatAffordably() {
-        if (this.balance >= 2.60) {
-            this.balance -= 2.60;
-        }
+    public void addMoney(double increase) {
+        this.balance += increase;
     }
 
-    public void eatHeartily() {
-        if (this.balance >= 4.60) {
-            this.balance -= 4.60;
+    public boolean takeMoney(double amount) {
+        // implement the method so that it only takes money from the card if
+        // the balance is at least the amount parameter.
+        // returns true if successful and false otherwise
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            return true;
         }
-    }
-
-    public void addMoney(double amount) {
-        double total;
-        double overage;
-        if (amount > 0) {
-            total = this.balance + amount;
-            overage = total - 150;
-            if (total > 150.00) {
-                total -= overage;
-            }
-            this.balance = total;
-        }
+        return false;
     }
 }
